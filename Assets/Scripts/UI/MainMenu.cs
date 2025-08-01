4eab84e22a90d291f89c8edffb9a2eee1cbedbc7
Andrew W. F. Hatton <7912262+Wanottah@users.noreply.github.com>
2025-08-01 16:56:52 +0100

    Commenting Code

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -8,39 +8,50 @@ public class MainMenu : MonoBehaviour
 
     public string firstLevel;
 
+    // reference to the menu 'continue' button
     public GameObject continueButton;
 
 
 
-    // Start is called before the first frame update
     void Start()
     {
+        // if 'playerprefs' has a 'continue' key entry
         if (PlayerPrefs.HasKey("CurrentLevel"))
         {
+            // if the 'playerprefs' key entry does not have a value
             if (PlayerPrefs.GetString("CurrentLevel") == "")
             {
+                // disable the 'continue' button
                 continueButton.SetActive(false);
             }
         }
 
+        // otherwise
+        // if 'playerprefs' doesn't have a 'continue' key entry
         else
         {
+            // disable the 'continue' button
             continueButton.SetActive(false);
         }
     }
 
 
+    // if the player has clicked the 'continue' button
     public void Continue()
     {
         SceneManager.LoadScene(PlayerPrefs.GetString("CurrentLevel"));
     }
 
 
+    // if the player has clicked the 'play'button
+    // start playing the game
     public void PlayGame()
     {
+        // initialise the game data
         InitialiseGameData();
 
 
+        // load the 'level1' game scene
         SceneManager.LoadScene(firstLevel);
 
         PlayerPrefs.SetString("CurrentLevel", "");
@@ -49,10 +60,13 @@ public class MainMenu : MonoBehaviour
     }
 
 
+    // initialise the game data
     private void InitialiseGameData()
     {
+        // set the player's current health in the game data instance
         GameData.gameDataInstance.currentHealth = 100;
 
+        // set the player's maximum health in the game data instance
         GameData.gameDataInstance.maximumHealth = 100;
     }
 
